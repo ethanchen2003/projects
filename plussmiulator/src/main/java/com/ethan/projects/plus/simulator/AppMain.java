b4467ec63ad018e0a036ee0b34a760f5b8d31bc1
Ethan Chen <ethan.chen2003@gmail.com>
2021-03-13 21:41:33 -0600

    Create AppMain.java
    add java code1

---
+++ plussmiulator/src/main/java/com/ethan/projects/plus/simulator/AppMain.java
@@ -0,0 +1,9 @@
+package com.ethan.projects.plus.simulator;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		PlusSimulator plusSimulator = new PlusSimulator();
+		plusSimulator.execute();
+	}
+}

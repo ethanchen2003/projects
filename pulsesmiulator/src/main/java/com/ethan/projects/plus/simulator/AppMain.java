7034c92b8e72c38ed20d36ae4414712fc6d83ef8
Ethan Chen <ethan.chen2003@gmail.com>
2021-03-14 15:35:20 -0500

    Delete AppMain.java

--- pulsesmiulator/src/main/java/com/ethan/projects/plus/simulator/AppMain.java
+++
@@ -1,9 +0,0 @@
-package com.ethan.projects.plus.simulator;
-
-public class AppMain {
-
-	public static void main(String[] args) {
-		PlusSimulator plusSimulator = new PlusSimulator();
-		plusSimulator.execute();
-	}
-}
